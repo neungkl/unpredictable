7c59954f0be366534fd50cb7d11797b08156ae84
kozartaeneung <kosatelim@gmail.com>
2015-04-25 23:36:15 +0700

    add gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var header = require('gulp-header');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
+var lessAutoPrefix = require('less-plugin-autoprefix');
+var lessCleanCss = require('less-plugin-clean-css');
+
+var isMin = false;
 
 var creditText = [
   '/* Develope by : Kosate Limpongsa',
@@ -14,23 +18,35 @@ var creditText = [
 
 
 gulp.task('js',function() {
-  gulp.src('./src/private/*.coffee')
+  var tmp =
+  gulp.src('./src/private/index.coffee')
     .pipe(coffeelint())
     .pipe(coffeelint.reporter())
     .pipe(coffee())
-    .on('error',function(){})
-    .pipe(uglify())
+    .on('error',function(){});
+  if( isMin )
+    tmp.pipe(uglify());
+  tmp
     .pipe(header(creditText))
     .pipe(gulp.dest('./src/public/'));
 });
 
 gulp.task('css',function() {
-  gulp.src('./src/private/*.less')
+  var tmp = gulp.src('./src/private/index.less')
     .pipe(less())
-    .on('error',function( err ){ console.log( err ) })
-    .pipe(minifyCss( { compatibility:'ie8' } ))
-    .pipe(header(creditText))
-    .pipe(gulp.dest('./src/public/'))
+    .on('error',function( err ){ console.log(err); });
+  if( isMin )
+    tmp
+      .pipe(minifyCss())
+      .pipe(header(creditText));
+  else
+    tmp.pipe(header(creditText));
+  tmp.pipe(gulp.dest('./src/public/'));
+});
+
+gulp.task('build',function() {
+  isMin = true;
+  gulp.start( 'js','css' );
 });
 
 gulp.task('default',['js','css']);
--- src/private/index.coffee
+++ src/private/index.coffee
@@ -1,2 +1,7 @@
 $ ->
-  console.log 555
+  canvas = $('canvas')
+  paper.setup( canvas )
+  path = new paper.Path()
+  path.moveTo( new paper.Point(100,100) )
+  path.lineTo( new paper.Point(200,200) )
+  paper.view.draw()
